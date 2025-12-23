5bb517778b755547cb32079a93fa77b2142b2d02
amdadisalm01 <amdadislam733@gmail.com>
2025-12-23 17:21:34 +0600

    vercel problem

--- src/api/axiosPublic.js
+++ src/api/axiosPublic.js
@@ -1,27 +1,21 @@
 import axios from "axios";
 
-const apiBase = import.meta.env.VITE_API_URL || "http://23.239.111.164:5001/api/v1";
-
-
-const proxy = "https://fresh-harvests-rose.vercel.app/";  
-
-const baseURL = import.meta.env.DEV 
-  ? "/api/api/v1"
-  : proxy;  
-
 const axiosPublic = axios.create({
-  baseURL,
+  baseURL: "/api/api/v1",
 });
 
 axiosPublic.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("access-token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
 );
+
 
 export default axiosPublic;
